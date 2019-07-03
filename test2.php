183e151eff8d94cf726b81aa0f009fd1ffadab23
work <2241841662@qq.com>
2019-07-03 18:13:06 +0800

    mm

--- test2.php
+++ test2.php
@@ -16,20 +16,48 @@
 
 class User
 {
+    /**
+     * log doc
+     */
     protected $log;
+    /**
+     * loglog doc
+     */
+    protected $loglog;
 
-    public function __construct(Log $log)
-    {
-        $this->log = $log;
-    }
+//    public function __construct(Log $log)
+//    {
+//        $this->log = $log;
+//    }
 
-    public function login()
+    public function login($b = 0)
     {
         // 登录成功，记录登录日志
-        echo 'login success...';
-        $this->log->write();
+        echo 'login success...'."$b";
+//        $this->log->write();
     }
 
 }
 
-var_dump(User::class);exit;
+
+
+
+$a = new ReflectionClass('User');
+$properties = $a->getProperties();   //返回一个由ReflectionProperty对象构成的数组
+$methods = $a->getMethods();         //返回ReflectionMethod对象构成的数组
+$instance  = $a->newInstanceArgs();
+
+
+foreach ($properties as $k=>$v){
+//    var_dump($v->getName());   //得到属性名字
+}
+foreach ($methods as $k=>$v){
+        $method_str = $v->getName();   //方法的名称字符串
+
+    $method = $a->getMethod($method_str);  //获取方法
+    $method->invoke($instance);   //执行方法
+    $para = $method->getParameters();
+    var_dump($para);exit;
+}
+
+//var_dump($para);exit;
